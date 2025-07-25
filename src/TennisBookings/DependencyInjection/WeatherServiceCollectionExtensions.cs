76b70dba252ca3913683c386a68691df4c3a41a6
Kiran <35651538+kiranm03@users.noreply.github.com>
2025-07-25 16:45:04 +1000

    Initial

---
+++ src/TennisBookings/DependencyInjection/WeatherServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using TennisBookings.External;
+using TennisBookings.Services.Weather;
+
+namespace TennisBookings.DependencyInjection;
+
+public static class WeatherServiceCollectionExtensions
+{
+	public static IServiceCollection AddWeatherForecasting(this IServiceCollection services,
+		IConfiguration config)
+	{
+		if (config.GetValue<bool>("Features:WeatherForecasting:EnableWeatherForecasting"))
+		{
+			services.AddHttpClient<IWeatherApiClient, WeatherApiClient>();
+			services.TryAddSingleton<IWeatherForecaster, WeatherForecaster>();
+			services.Decorate<IWeatherForecaster, CachedWeatherForecaster>();
+		}
+		else
+		{
+			services.TryAddSingleton<IWeatherForecaster, DisabledWeatherForecaster>();
+		}
+
+		return services;
+	}
+}
